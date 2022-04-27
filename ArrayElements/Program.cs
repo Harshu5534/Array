6f99f0ea63624137fd5058d59cee925a5ab3e3c8
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-27 17:33:31 +0530

    [Harshal]Add:Find Maximum float value in array

--- ArrayElements/Program.cs
+++ ArrayElements/Program.cs
@@ -6,9 +6,12 @@ namespace ArrayElements
         static void Main(string[] args)
         {
             int[] intArr = { 1, 10, 4, 8, 9 };
+            float[] floatArr = { 2.33f, 5.33f, 4.55f, 6.88f, 7.88f };
             Array array = new Array();
-            Console.WriteLine("--------It is Integer type value in array------");
+            Console.WriteLine("\n--------It is Integer type value in array------\n");
             array.Toprint(intArr);
+            Console.WriteLine("\n--------It is Float type value in array------\n");
+            array.Toprint(floatArr);
         }
     }
 }
